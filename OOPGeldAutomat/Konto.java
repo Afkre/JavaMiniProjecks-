85f32609c5fd4fda9fc6b3111d7aa6dec7f5587b
Afkre <ahmetefkere@gmail.com>
2023-05-08 23:11:37 +0200

    2023.05.08-2

--- OOPGeldAutomat/Konto.java
+++ OOPGeldAutomat/Konto.java
@@ -1,3 +1,42 @@
+import java.lang.reflect.Constructor;
+
 public class Konto {
+        private String benutzerName;
+        private String passwort;
+        private int bilanz;
+
+        public String getPasswort() {
+            return passwort;
+        }
+
+        public void setPasswort(String passwort) {
+            this.passwort = passwort;
+        }
+
+        
+
+        public String getBenutzerName() {
+            return benutzerName;
+        }
+
+        public void setBenutzerName(String benutzerName) {
+            this.benutzerName = benutzerName;
+        }
+
+        public int getBilanz() {
+            return bilanz;
+        }
+
+        public void setBilanz(int bilanz) {
+            this.bilanz = bilanz;
+        }
+
+        public Konto(String benutzerName, String pass, int bilanz){
+            this.benutzerName= benutzerName;
+            this.passwort= passwort;
+            this.bilanz = bilanz;
+        }
+
+
     
 }

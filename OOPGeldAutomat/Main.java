1b01d68e7fbc2df228bfd7beb7a95b04adaa3399
Afkre <ahmetefkere@gmail.com>
2023-05-08 22:54:01 +0200

    2023.05.08-1

---
+++ OOPGeldAutomat/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main (String[]args){
+        // Object Oriented Programming - GeldAutomat
+
+        // Login Class-> kontrolliert die Benutzereingaben
+
+        // Konto Class-> erledigt die Kontotransactionen
+
+        // ATM Class -> wird den Bankautomaten bedienen.
+
+
+
+    }
+    
+
+}

322d73913c5ecf7636f181d13c55cbd560793542
Afkre <ahmetefkere@gmail.com>
2023-05-09 23:54:57 +0200

    2023.05.09-3

--- OOPGeldAutomat/GeldAutomat.java
+++ OOPGeldAutomat/GeldAutomat.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class GeldAutomat {
-    public void arbeit(){
+    public void arbeit(Konto konto){
+
+        Login login = new Login();
         
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Herzlich willkommen bei unserer Bank...");
+
+        System.out.println("****************************************");
+
+        System.out.println("Benutzer EinLogin");
+
+        System.out.println("****************************************");
+
+        int einloginrecht = 3;
+
+        while (true){
+            if (login. login(konto)){
+                System.out.println("Erfolgreiherlige Einlogin");
+                break;
+            }else{
+                System.out.println("unerfolgreichliche Einlogin");
+                int einloginrecht = -=1;
+
+                System.out.println("verbleibender Einlogin:  " + login );
+            }
+        }
     }
 }
--- OOPGeldAutomat/Main.java
+++ OOPGeldAutomat/Main.java
@@ -1,14 +1,15 @@
 public class Main{
     public static void main (String[]args){
-        
+
         // Object Oriented Programming - GeldAutomat
         // Login Class-> kontrolliert die Benutzereingaben
         // Konto Class-> erledigt die Kontotransactionen
         // Geldautomat Class -> wird den Bankautomaten bedienen.
 
         GeldAutomat geldAutomat = new GeldAutomat();
+        Konto konto = new Konto ("Ahmet Efkere", "123456", 2500)
 
-        geldAutomat.arbeit();
+        geldAutomat.arbeit(konto);
 
         System.out.println("Ausstieg aus dem Programm!..");
 

03febec4f36c11a467c611d6b345b5b372535043
Afkre <ahmetefkere@gmail.com>
2023-05-13 23:48:25 +0200

    2023051323

---
+++ TrainingProgramm/Main.java
@@ -0,0 +1,13 @@
+package TrainingProgramm;
+
+public class Main {
+    public static void main(String[]args){
+
+        //Training Programm erstellen
+        
+
+
+
+    } 
+    
+}
---
+++ TrainingProgramm/Training.java
@@ -0,0 +1,55 @@
+package TrainingProgramm;
+
+public class Training {
+    private int burpeezahl;
+    private int puschupzahl;
+    private int situpzahl;
+    private int squatzahl;
+
+    //Constructor
+    public Training(int burpeezahl, int puschupzahl, int situpzahl, int squatzahl) {
+        this.burpeezahl = burpeezahl;
+        this.puschupzahl = puschupzahl;
+        this.situpzahl = situpzahl;
+        this.squatzahl = squatzahl;
+    }
+
+    //Getter && Setter
+
+    public int getBurpeezahl() {
+        return burpeezahl;
+    }
+
+    public void setBurpeezahl(int burpeezahl) {
+        this.burpeezahl = burpeezahl;
+    }
+
+    public int getPuschupzahl() {
+        return puschupzahl;
+    }
+
+    public void setPuschupzahl(int puschupzahl) {
+        this.puschupzahl = puschupzahl;
+    }
+
+    public int getSitupzahl() {
+        return situpzahl;
+    }
+
+    public void setSitupzahl(int situpzahl) {
+        this.situpzahl = situpzahl;
+    }
+
+    public int getSquatzahl() {
+        return squatzahl;
+    }
+
+    public void setSquatzahl(int squatzahl) {
+        this.squatzahl = squatzahl;
+    }
+
+    
+
+
+
+}

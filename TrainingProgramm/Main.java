7ed1cb5156d9489b992eb14cc2d7f7102eeb8013
Afkre <ahmetefkere@gmail.com>
2023-05-14 23:53:29 +0200

    202305142300

--- TrainingProgramm/Main.java
+++ TrainingProgramm/Main.java
@@ -1,10 +1,25 @@
 package TrainingProgramm;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[]args){
 
         //Training Programm erstellen
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Willkomen zum Trainingsprogramm");
+
+        String trainingen ="Gültige Arten ...\n"
+                            + "Burpee\n"
+                            + "Pushup\n"
+                            + "Situp\n"
+                            + "Squat";
+        System.out.println(trainingen);
+
+        System.out.println("Erstellen Sie eine Training...");
+
+        System.out.println();
 
 
 

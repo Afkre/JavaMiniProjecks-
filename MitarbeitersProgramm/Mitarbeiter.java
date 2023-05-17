15ee1414a7d6058807427f60e3e79cc2690f7be5
Afkre <ahmetefkere@gmail.com>
2023-05-17 23:27:35 +0200

    2305172315

---
+++ MitarbeitersProgramm/Mitarbeiter.java
@@ -0,0 +1,40 @@
+package MitarbeitersProgramm;
+
+public class Mitarbeiter {
+    private String vorname;
+    private String nachname;
+    private int id;
+
+    public Mitarbeiter(String vorname, String nachname, int id) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.id = id;
+    }
+
+    public void anzeigenInfornationen(){
+        System.out.println("Vorname : " + vorname);
+        System.out.println("Nachname : " + nachname);
+        System.out.println("ID : " + id);
+        
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+    public String getNachname() {
+        return nachname;
+    }
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}

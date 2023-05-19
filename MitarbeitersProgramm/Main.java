84e879e09f6ed0e27772dd3a3e688b39f17c49b4
Afkre <ahmetefkere@gmail.com>
2023-05-19 23:21:21 +0200

    2305192200

--- MitarbeitersProgramm/Main.java
+++ MitarbeitersProgramm/Main.java
@@ -1,5 +1,15 @@
 package MitarbeitersProgramm;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        
     
+    Scanner scanner = new Scanner (System.in);
+    System.out.println("Herzlichen Willkommen, zum MitarbeitersProgramm");
+
+    
+    }
 }

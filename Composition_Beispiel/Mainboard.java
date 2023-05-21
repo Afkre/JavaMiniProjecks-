f73ca4b339126f2c07a67c804aa4e4b033a706ac
Afkre <ahmetefkere@gmail.com>
2023-05-21 23:58:24 +0200

    2305212330

--- Composition_Beispiel/Mainboard.java
+++ Composition_Beispiel/Mainboard.java
@@ -2,4 +2,56 @@ package Composition_Beispiel;
 
 public class Mainboard {
     
+    private String model;
+    
+    private String hersteller;
+
+    private int schnittstelleZahl;
+
+    private String betriebsSystem;
+
+    public Mainboard(String model, String hersteller, int schnittstelleZahl, String betriebsSystem) {
+        this.model = model;
+        this.hersteller = hersteller;
+        this.schnittstelleZahl = schnittstelleZahl;
+        this.betriebsSystem = betriebsSystem;
+    }
+
+    
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getHersteller() {
+        return hersteller;
+    }
+
+    public void setHersteller(String hersteller) {
+        this.hersteller = hersteller;
+    }
+
+    public int getSchnittstelleZahl() {
+        return schnittstelleZahl;
+    }
+
+    public void setSchnittstelleZahl(int schnittstelleZahl) {
+        this.schnittstelleZahl = schnittstelleZahl;
+    }
+
+    public String getBetriebsSystem() {
+        return betriebsSystem;
+    }
+
+    public void setBetriebsSystem(String betriebsSystem) {
+        this.betriebsSystem = betriebsSystem;
+    }
+    
+
+
+    
 }

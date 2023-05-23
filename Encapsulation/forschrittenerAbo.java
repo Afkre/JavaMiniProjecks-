6eb6cf991f5e523ff8773c824126332afae1f9ba
Afkre <ahmetefkere@gmail.com>
2023-05-23 23:58:14 +0200

    2305232330

--- Encapsulation/forschrittenerAbo.java
+++ Encapsulation/forschrittenerAbo.java
@@ -21,6 +21,28 @@ public class forschrittenerAbo {
         this.stadt = stadt;
     }
 
+    public void erdgasbenutz(int betrag){
+        if ((this.restbetrag - betrag ) < 0){
+
+            System.out.println("Kein genung Restbetrag");
+
+        } else {
+            this.restbetrag-=betrag;
+            if (this.restbetrag <= 0){
+                System.out.println("Kein Restbetrag. Bitte laden Sie Guthaben auf."
+                +"Guthaben Limit ist max. 120 Euro"
+                );
+            } else {
+                System.out.println("Neue Betrag :" + restbetrag);
+            }
+        }
+
+    }
+
+    public void restbetraganfragen(){
+
+        System.out.println("Restbetrag = " + restbetrag);
+
     
 
    

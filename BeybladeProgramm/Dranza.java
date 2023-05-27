a9e1443683ee4eec5c4d9024fa7eb8252decc6ac
Afkre <ahmetefkere@gmail.com>
2023-05-27 16:33:13 +0200

    2305271600

--- BeybladeProgramm/Dranza.java
+++ BeybladeProgramm/Dranza.java
@@ -1,5 +1,31 @@
 package BeybladeProgramm;
 
-public class Dranza {
+public class Dranza extends Beyblade{
+
+    private String heiligeBestie;
+
+    public Dranza(String beybladehat, int drehzahl, int angriffskraft, String heiligeBestie) {
+        super(beybladehat, drehzahl, angriffskraft);
+        this.heiligeBestie = heiligeBestie;
+    }
+
+    @Override
+    public void anzeigtInformationen() {
+        super.anzeigtInformationen();
+
+        System.out.println("Names der heiligen Bestie: "+ heiligeBestie);
+    }
+
+    @Override
+    public void entfessleHeiligeBestie() {
+        //super.entfessleHeiligeBestie();
+        System.out.println(getBeybladehat()+ " entfellelt "+ heiligeBestie);
+        System.out.println("Angriff der"+  getBeybladehat()+ " : Flammenschwert");
+
+    }
+
+    
+    
+    
     
 }

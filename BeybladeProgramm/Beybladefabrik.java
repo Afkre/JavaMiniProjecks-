59f3317043eaa865628d4ce98fa4728fb39f2526
Afkre <ahmetefkere@gmail.com>
2023-05-27 23:16:19 +0200

    2305272315

--- BeybladeProgramm/Beybladefabrik.java
+++ BeybladeProgramm/Beybladefabrik.java
@@ -5,18 +5,18 @@ public class Beybladefabrik {
     public Beyblade beyblademacht(String artBeyblade){
 
         if (artBeyblade.equals("Dragon")){
-            return new Dragon("Takao", 800, 300, "blauer Drache", "Das Gespräch mit der heiligen Bestie");
+            return new Dragon("Takao ", 800, 300, " Blauer Drache", " Das Gespräch mit der heiligen Bestie");
         }
         else if(artBeyblade.equals("Dranza")){
-            return new Dranza("Kai", 600, 400, "Roter Phonix");
+            return new Dranza("Kai ", 600, 400, " Roter Phonix");
 
         }
         else if(artBeyblade.equals("Drayga")){
-            return new Drayga("Rei",800, 1000,  "Weisser Tiger");
+            return new Drayga("Rei ",800, 1000,  " Weisser Tiger");
         
         }
         else if(artBeyblade.equals("Draciel")){
-            return new Draciel("Max",400, 1000,  "Schwarze Schildkröte ");
+            return new Draciel("Max ",400, 1000,  " Schwarze Schildkröte ");
         }
         return null;
         

5dc915a9d308472b330ad8e5b8ac2777cdab24aa
Afkre <ahmetefkere@gmail.com>
2023-06-04 10:05:56 +0200

    2306041006

--- SingerList/Test.java
+++ SingerList/Test.java
@@ -93,7 +93,7 @@ public class Test {
                     break;
                 case 4:
                     delete();
-                    break;6
+                    break;
                 case 5:
                     search();
                     break;

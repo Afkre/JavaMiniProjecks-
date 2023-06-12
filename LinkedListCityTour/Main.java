27c0e0b603c1e0469d1737ad8714e097c644d2c6
Afkre <ahmetefkere@gmail.com>
2023-06-12 23:43:09 +0200

    2306122300

--- LinkedListCityTour/Main.java
+++ LinkedListCityTour/Main.java
@@ -2,6 +2,7 @@ package LinkedListCityTour;
 
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
 
@@ -21,6 +22,83 @@ public class Main {
         int transaktion;
 
         transaktionenDruck();
+         Scanner scanner = new Scanner(System.in);
+        
+        if (!iterator.hasNext()) {
+            
+            System.out.println("Es gibt keine Städte...");
+        }
+        boolean ausgang = false;
+        boolean next = true;
+        
+        while (!ausgang) {
+            System.out.println("Wählen Sie eine Transaktion:");
+            
+            transaktion = scanner.nextInt();
+            
+            switch(transaktion) {
+                case 0:
+                    transaktionenDruck();
+                    break;
+                case 1:
+                    if (!next) {
+                        if (iterator.hasNext()) {
+                            
+                            iterator.next();
+                            
+                        }
+                        next = true;
+                        
+                    }
+                    if (iterator.hasNext()) {
+                        
+                        System.out.println("Anreise in die Stadt " + iterator.next());
+                       
+                    }
+                    else {
+                        System.out.println("Gidilecek Şehir Kalmadı...");
+                        next = true;
+                        
+                        
+                    }
+                    break;
+                case 2:
+                    if (next){
+                        if (iterator.hasPrevious()) {
+                            iterator.previous();
+                            
+                        }
+                        next = false;
+                        
+                    }
+                    if (iterator.hasPrevious()) {
+                        
+                        System.out.println("Anreise zur Stadt : " + iterator.previous());
+                        
+                    }
+                    else {
+                        System.out.println("Die Citiestour hat begonnen....");
+                    }
+                    break;
+                    
+                case 3:
+                    ausgang = true;
+                    System.out.println("Die Anwendung wird aufgegeben....");
+                    break;
+                
+                
+                
+                
+            }
+            
+            
+            
+            
+        }
+        
+        
+        
+    }
 
 
 

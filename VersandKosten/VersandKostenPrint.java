fe7f19aed8471d9ed177876b827fe107edf51707
Afkre <ahmetefkere@gmail.com>
2023-06-13 23:50:14 +0200

    2306132330

--- VersandKosten/VersandKostenPrint.java
+++ VersandKosten/VersandKostenPrint.java
@@ -6,12 +6,11 @@ public class VersandKostenPrint {
     public static void main(String[]args) {
     
     double gewicht;
-        Scanner tastatur = new Scanner(System.in);
-        System.out.print("Die Versandgebühr wird nach dem Gewicht der Sendung berechnet.\nBitte geben Sie das Gewicht(als Gramm) der Sendung ein =");
-        gewicht = tastatur.nextDouble();
-
     
-
+        try (Scanner tastatur = new Scanner(System.in)) {
+            System.out.print("Die Versandgebühr wird nach dem Gewicht der Sendung berechnet.\nBitte geben Sie das Gewicht(als Gramm) der Sendung ein =");
+            gewicht = tastatur.nextDouble();
+        }
         VersandKosten post1 = new VersandKosten();
         //post1.gewicht = gewicht;
         System.out.println("Die Versandkosten für eine Sendung mit dem angegebenen Gewicht= "+post1.sumCost(gewicht)+" Euro");

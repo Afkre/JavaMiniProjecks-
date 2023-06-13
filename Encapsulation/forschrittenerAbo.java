361f690c29553b909dece45620bd12cb8d47daa9
Afkre <ahmetefkere@gmail.com>
2023-06-13 23:52:53 +0200

    2306132335

--- Encapsulation/forschrittenerAbo.java
+++ Encapsulation/forschrittenerAbo.java
@@ -4,21 +4,13 @@ package Encapsulation;
 
 public class forschrittenerAbo {
 
-    private String name;
-
     private int restbetrag = 120 ;
 
-    private String stadt;
-
     public forschrittenerAbo(String name, int restbetrag, String stadt) {
-        this.name = name;
-
         if (restbetrag >= 0 && restbetrag<= 120 ){
             this.restbetrag = restbetrag;
 
         }
-
-        this.stadt = stadt;
     }
 
     public void erdgasbenutz(int betrag){

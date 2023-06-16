802e7cd31bc0f10b7ae0898d637e628c02eaf443
Afkre <ahmetefkere@gmail.com>
2023-06-16 23:57:01 +0200

    2306162300

--- AusreiseProgramm/Main.java
+++ AusreiseProgramm/Main.java
@@ -1,5 +1,8 @@
 package AusreiseProgramm;
 
 public class Main {
+    public static void main(String[] args) throws InterruptedException{
+        System.out.println("Herzlichen Willkommen am Flughafen München");
+    }
     
 }
--- AusreiseProgramm/Reisende.java
+++ AusreiseProgramm/Reisende.java
@@ -36,4 +36,5 @@ public class Reisende implements RegelnAusland{
         
     }
     
+    
 }

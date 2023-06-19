2386303ad6105f87fd06683e8cb452cc0949451c
Afkre <ahmetefkere@gmail.com>
2023-06-19 23:58:18 +0200

    2306192345

--- TaschenRechner/Main.java
+++
@@ -1,5 +0,0 @@
-package TaschenRechner;
-
-public class Main {
-    
-}
---
+++ Zahlfinden/Main.java
@@ -0,0 +1,23 @@
+package Zahlfinden;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] zahlen = new int[] {1,2,5,7,9,0};
+            int suchen = 5;
+            boolean gibt=false;
+
+        for(int zahl : zahlen){
+            if(zahl==suchen){
+                gibt=true;
+                break;
+            }
+        }
+
+        if(gibt){
+            System.out.println("Zahl verfügbar");
+        }else {
+            System.out.println("Zahl verfügbarnicht");
+        }
+    }
+    
+}

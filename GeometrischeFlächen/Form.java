67460659f926208d3dbb327a46cfefe4aa5c372d
Afkre <ahmetefkere@gmail.com>
2023-06-20 23:55:41 +0200

    2306202330

--- GeometrischeFlächen/Form.java
+++ GeometrischeFlächen/Form.java
@@ -5,7 +5,7 @@ public abstract class Form {
     private String name;
 
     public Form (String name){
-        this.name=name
+        this.name = name;
     }
 
     abstract void flaechenRechner();

cd8f28502de5b53fa8414e96be2f4c50e07f72b2
Afkre <ahmetefkere@gmail.com>
2023-06-21 23:42:33 +0200

    2306212300

--- GeometrischeFlächen/Dreieck.java
+++ GeometrischeFlächen/Dreieck.java
@@ -1,5 +1,29 @@
 package GeometrischeFlächen;
 
-public class Dreieck {
+public class Dreieck extends Form{
+    private int a;
+    private int b;
+    private int c;
+
+    public Dreieck(String name, int a, int b, int c){
+        super(name);
+        this.a=a;
+        this.b=b;
+        this.c=c;
+
+    }
+
+
+    @Override
+    void flaechenRechner() {
+    
+        double u = (a+b+c)/2.0;
+
+        double flaeche= Math.sqrt(u*(u-a)*(u-b)*(u-c));
+        System.out.println();
+        
+    }
+
+    
     
 }

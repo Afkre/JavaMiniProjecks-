936089a2ded23d22396c007e61838acfb6a5bf6e
Afkre <ahmetefkere@gmail.com>
2023-06-23 23:30:49 +0200

    2306232300

--- GeometrischeFlächen/Quadrat.java
+++ GeometrischeFlächen/Quadrat.java
@@ -1,5 +1,17 @@
 package GeometrischeFlächen;
 
-public class Quadrat {
+public class Quadrat extends Form{
+    private int kante;
+
+
+    public Quadrat(String name, int kante){
+        super(name);
+        this.kante= kante;
+    }
+
+    @Override
+    void flaechenRechner(){
+
+    }
     
 }

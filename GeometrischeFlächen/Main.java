764331a8e236652d5dcfa41ae467267d1598d385
Afkre <ahmetefkere@gmail.com>
2023-06-25 19:57:59 +0200

    2306251700

--- GeometrischeFlächen/Main.java
+++ GeometrischeFlächen/Main.java
@@ -1,5 +1,16 @@
 package GeometrischeFlächen;
 
+import java.util.Scanner;
+
 public class Main {
+    
+    Scanner scanner = new Scanner(System.in);
+
+    String transactions = "Transactions...\n"
+                          +"1.Quadratischer Flächenrechner\n"
+                          +"2.Dreieckischer Flächenrechner\n"
+                          +"3.Kreisischer Flächenrechner\n"
+                          +"q:Ausgang";
+
     
 }

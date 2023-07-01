9882fe9a620424bee412c50e8dffb2ec5c4115db
Afkre <ahmetefkere@gmail.com>
2023-07-01 23:38:21 +0200

    2307012300

--- ProblemLoesungen/Problem.java
+++ ProblemLoesungen/Problem.java
@@ -3,6 +3,9 @@ package ProblemLoesungen;
 public class Problem {
 
     public static class Mathematik{
+        public static void Kreisflache(int  radius){
+            System.out.println("Kreisflaechen: " + (Math.PI * radius * radius);
+        }
 
     }
 

ed85a24d8d15748e6db890e1a0a2b46e74fbdfda
Afkre <ahmetefkere@gmail.com>
2023-07-04 23:55:36 +0200

    2307042330

--- ProblemLoesungen/Main.java
+++ ProblemLoesungen/Main.java
@@ -7,5 +7,10 @@ public class Main {
         
         Scanner scanner = new Scanner(System.in);
         System.out.println("Willkommen beim Programm für mathematische und physikalische Probleme");
+        String transactions = "Transactions ...\n"
+                            + "1. Berechnung der Kreisfläche\n" 
+                            + "2. Berechnung des Dreiecksumfangs\n" 
+                            + "3. Berechne das inverse Produkt zweier Vektoren"
+                            + "Beenden: q ";
     }
 }

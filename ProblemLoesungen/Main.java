ce60b13e23269ab6c8ee1688922747855f6ec1ff
Afkre <ahmetefkere@gmail.com>
2023-07-05 00:23:05 +0200

    2307050015

--- ProblemLoesungen/Main.java
+++ ProblemLoesungen/Main.java
@@ -20,8 +20,46 @@ public class Main {
             String transaction = scanner.nextLine();
 
             if (transaction.equals("q")){
-                System.out.println("");
+                System.out.println("Beenden des Programms");
+                break;
             }
+            else if (transaction.equals("1")){
+                System.out.print("Radius der Kreisfläche : ");
+                int radius = scanner. nextInt();
+                scanner.nextLine();
+
+                Problem.Mathematik.kreisflache(radius);
+                
+                scanner.close();
+
+            }
+            else if (transaction.equals("2")){
+
+                System.out.print("Seite 1: ");
+                int seite1 = scanner.nextInt();
+                System.out.print("Seite 2: ");
+                int seite2 = scanner.nextInt();
+                System.out.print("Seite 3: ");
+                int seite3 = scanner.nextInt();
+                scanner.nextLine();
+
+                Problem.Mathematik.umfangDreieck(seite1, seite2, seite3);
+
+                scanner.close();
+
+            }
+            else if (transaction.equals("3")){
+                Vec vec1 = new Vec("Vector1");
+                Vec vec2 = new Vec("Vector2");
+
+                Problem.Physik.inneresMultiplikation(vec1, vec2);
+
+
+            }
+            else {
+                System.out.println("ungültige Transaction...");
+            }
+            
         }
 
     }

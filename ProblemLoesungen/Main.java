eafe346953636d02aef9260d5f586d2c3f6d698f
Afkre <ahmetefkere@gmail.com>
2023-07-04 23:49:29 +0200

    2307042300

--- ProblemLoesungen/Main.java
+++ ProblemLoesungen/Main.java
@@ -1,7 +1,11 @@
 package ProblemLoesungen;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Willkommen beim Programm für mathematische und physikalische Probleme");
     }
 }

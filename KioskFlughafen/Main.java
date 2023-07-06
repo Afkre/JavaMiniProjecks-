8f41458c2f0ef451624cac960368b85b0c8655fa
Afkre <ahmetefkere@gmail.com>
2023-07-07 00:18:27 +0200

    2307070000

--- KioskFlughafen/Main.java
+++ KioskFlughafen/Main.java
@@ -26,34 +26,48 @@ public class Main {
             System.out.println(bedingungen);
             
             Fluggast fluggast = new Fluggast();
+
+            System.out.println("Der Visastatus wird überprüft.");
+
             Thread.sleep(3000);
-            
             if (fluggast.visumStatusKontroll()==false){
 
                 System.out.println(message);
                 continue;
             }
 
-            
+            System.out.println("Die Gültigkeit des Reisepasses wird überprüft.");
 
-
+            Thread.sleep(3000);
             if (fluggast.gültigkeitRPassesKontroll()==false){
 
                 System.out.println(message);
                 continue;
             }
 
+
+            System.out.println("Es wird geprüft, ob eine Ausreiseverbote bestehen..");
+
+            Thread.sleep(3000);
             if (fluggast.ausReiseVerbotKontroll()==false){
 
                 System.out.println(message);
                 continue;
             }
 
+            System.out.println("Es wird geprüft, ob außer dem Handgepäck noch anderes Gepäck vorhanden ist.");
+
+            Thread.sleep(3000);
             if (fluggast.gepaeckKontroll()==false){
 
                 System.out.println(message);
                 continue;
             }
+
+            
+            System.out.println("Es wird geprüft, ob das Handgepäck ein angemessenes Gewicht hat.");
+
+            Thread.sleep(3000);
 
             if (fluggast.kabinGepaeckKontroll()==false){
 
@@ -61,7 +75,7 @@ public class Main {
                 continue;
             }
 
-
+            System.out.println("Ihre Verfahren sind abgeschlossen! Sie können Ihre Bordkarte für den Flug erhalten.");
 
 
         }

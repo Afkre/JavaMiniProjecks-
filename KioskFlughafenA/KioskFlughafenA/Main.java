4768fb728bd19ecc32ab011a973b96ac2193da23
Afkre <ahmetefkere@gmail.com>
2023-07-11 23:55:26 +0200

    2307112300

--- KioskFlughafenA/KioskFlughafenA/Main.java
+++ KioskFlughafenA/KioskFlughafenA/Main.java
@@ -12,7 +12,7 @@ public class Main {
                       +"Der Fluggast muss über ein gültiges Visum für das Zielland verfügen...\n"
                       +"Der Fluggast muss im Besitz eines gültigen Reisepasses sein.  Achten Sie bitte auf das Ablaufdatum...\n"
                       +"Gegen den Fluggast darf kein gerichtliches Ausreiseverbot verhängt worden sein. Dies wird bei der Polizeikontrolle erneut überprüft...\n"
-                      +("Der Fluggast darf kein anderes Gepäck als Handgepäck mitführen...\n")
+                      +"Der Fluggast darf kein anderes Gepäck als Handgepäck mitführen...\n"
                       +"Das Handgepäck des Fluggastes darf 8 Kilogramm nicht überschreiten...\n";
 
         

624648d14b3bac598b6c49d8bb5b51e564f95d61
Afkre <ahmetefkere@gmail.com>
2023-08-08 00:20:52 +0200

    2308080000

--- Notfalldienst/Patient.java
+++ Notfalldienst/Patient.java
@@ -1,5 +1,28 @@
 package Notfalldienst;
 
-public class Patient {
+public class Patient implements Comparable<Patient>{
+
+    private String name;
+    private String beschwerde;
+
+    private int prioritaet;
+
+    public Patient(String name, String beschwerde){
+        this.name = name;
+        this.beschwerde = beschwerde;
+        
+        if (beschwerde.equals("Blinddarmentzündung")){
+            this.prioritaet =3;
+            
+        }else if (beschwerde.equals("Verbrennung")){
+            this.prioritaet =3;
+
+        }else {
+            this.prioritaet = 1;
+        }
+    }
+    
+
+
     
 }

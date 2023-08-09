9a4002d2591e07fb32c4a9f2fc7e0e056a11d51b
Afkre <ahmetefkere@gmail.com>
2023-08-09 11:23:55 +0200

    2308091000

--- Notfalldienst/Main.java
+++ Notfalldienst/Main.java
@@ -1,5 +1,47 @@
 package Notfalldienst;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
 public class Main {
-    
+    public static void main(String[] args) {
+        // Notdienstanwendung
+        
+        /*
+        Die Patienten werden entsprechend ihrer Beschwerden in der Notaufnahme an den Anfang der Warteschlange gestellt (PriorityQueue).
+        
+        Blinddarmentzündung ----> Höchste Priorität
+        Verbrennung -------> Mittlere Priorität
+        Kopfschmerzen ---> Geringste Priorität
+        
+        
+        new Patient("Herr Murat", "Verbrennung")
+        
+        
+        
+        
+        
+        */
+
+        Queue<Patient> notdienst = new PriorityQueue<Patient>();
+        
+        notdienst.offer( new Patient("Adam","Verbrennung"));
+        notdienst.offer( new Patient("David","Kopfschmerzen"));
+        notdienst.offer(new Patient("Anna","Blinddarmentzuendung"));
+        notdienst.offer( new Patient("Ute","Verbrennung"));
+        notdienst.offer(new Patient("Bill","Verbrennung"));
+        notdienst.offer( new Patient("Abraham","Blinddarmentzuendung"));
+        
+        int i = 1;
+        
+        while(notdienst.isEmpty() != true) {
+            System.out.println("**************************************");
+            System.out.println(i + ".");
+            System.out.println(notdienst.poll());
+            System.out.println("**************************************");
+            i++;
+            
+        }
+    }
 }
--- Notfalldienst/Patient.java
+++ Notfalldienst/Patient.java
@@ -12,16 +12,45 @@ public class Patient implements Comparable<Patient>{
         this.beschwerde = beschwerde;
         
         if (beschwerde.equals("Blinddarmentzündung")){
-            this.prioritaet =3;
+            this.prioritaet = 3;
             
         }else if (beschwerde.equals("Verbrennung")){
-            this.prioritaet =3;
+            this.prioritaet = 2;
 
         }else {
             this.prioritaet = 1;
         }
     }
-    
+
+    @Override
+    public String toString() {
+
+        String informationen =  " Name : " + name
+                            + "\nBeschwerde : " + beschwerde 
+                            +"\nPriority : " + prioritaet;
+                            
+       return informationen;
+       
+    }
+
+    @Override
+    public int compareTo(Patient patient) {
+
+        if (this.prioritaet > patient.prioritaet) {
+            
+            return -1;
+            
+        }
+        else if (this.prioritaet < patient.prioritaet) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+
+    }
+
+
 
 
     
